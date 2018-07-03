c180e045cfab2f9d735ca41a9f2fe98bf45e1980
Hadi Kefayati <h.kefaiati2010@gmail.com>
2018-07-04 00:01:31 +0430

    update attachment directive example

--- examples/pages/directives/attachment/attachment.controller.js
+++ examples/pages/directives/attachment/attachment.controller.js
@@ -3,16 +3,16 @@
         .module('test')
         .controller('AttachmentDirectiveController', AttachmentDirectiveController);
 
-    AttachmentDirectiveController.$inject = ['$http', 'alertService', 'loadingService'];
-    function AttachmentDirectiveController($http, alertService, loadingService) {
+    AttachmentDirectiveController.$inject = ['$http', 'alertService', 'loadingService', 'ObjectService'];
+    function AttachmentDirectiveController($http, alertService, loadingService, ObjectService) {
         var example = this;
 
-        example.tab = 1;
-        example.ID = 'd1bdf873-2312-4f3a-9678-418bac96c9d7';
-        example.save = save;
-        example.load = load;
-        example.attachment = {
-            bindings: [{ model: example, parameter: 'ID', as: 'ParentID' }]
+        example.main = new ObjectService();
+        example.main.model.ID = 'd1bdf873-2312-4f3a-9678-418bac96c9d7';
+        example.main.save = save;
+        example.main.load = load;
+        example.main.attachment = {
+            bindings: [{ obj: example.main, parameter: 'ID', as: 'ParentID' }]
             , autoSave: false
             , type: 2
             , uploadService: upload // should be a function that returns a promise
@@ -41,11 +41,11 @@
             return $http({
                 method: 'POST'
                 , url: 'http://localhost:26089/Attachment/Save'
-                , data: example.attachment.getParams()
+                , data: example.main.attachment.getParams()
             }).then(function (result) {
                 loadingService.hide();
                 alertService.success('فایل با موفقیت ذخیره شد');
-                example.attachment.bindingObject = result.data.Data;
+                example.main.attachment.bindingObject = result.data.Data;
             }).catch(function (error) {
                 loadingService.hide();
                 alertService.error('خطا در ذخیره‌سازی فایل');
@@ -56,32 +56,32 @@
             return $http({
                 method: 'POST'
                 , url: 'http://localhost:26089/Attachment/List'
-                , data: { ParentID: example.ID }
+                , data: { ParentID: example.main.model.ID }
             }).then(function (result) {
-                example.attachment.bindingObject = result.data.Data[0];
+                example.main.attachment.bindingObject = result.data.Data[0];
                 loadingService.hide();
             });
         }
-        function download() {
+        function download(model) {
             loadingService.show();
             let downloadWindow = window.open();
             return $http({
                 method: 'POST'
                 , url: 'http://localhost:26089/Attachment/Get'
-                , data: { ID: example.attachment.bindingObject.ID }
+                , data: model
             }).then(function (result) {
                 downloadWindow.location = 'http://localhost:26089/TemporaryFiles/' + result.data.Data.FileName;
                 loadingService.hide();
             });
         }
-        function remove() {
+        function remove(model) {
             loadingService.show();
             return $http({
                 method: 'POST'
                 , url: 'http://localhost:26089/Attachment/Delete'
-                , data: { ID: example.attachment.bindingObject.ID }
+                , data: model
             }).then(function () {
-                example.attachment.reset();
+                example.main.attachment.reset();
                 loadingService.hide();
             });
         }
