a05c3d2e054f28bf2d4966e41e69fd73d97b7f1e
Hadi Kefayati <h.kefaiati2010@gmail.com>
2018-11-20 21:30:40 +0330

    handle 401 error

--- src/services/auth-interceptor.service.js
+++ src/services/auth-interceptor.service.js
@@ -1,63 +1,34 @@
-﻿authInterceptorService.$inject = ['$q', '$injector', '$cookies', 'globalService'];
-export default function authInterceptorService($q, $injector, $cookies, globalService) {
-    const service = {
-        request: request
-        , responseError: responseError
+﻿authInterceptorService.$inject = ["$q", "$timeout", "globalService"];
+export default function authInterceptorService($q, $timeout, globalService) {
+  const service = {
+    request: request,
+    responseError: responseError
+  };
+
+  return service;
+
+  function request(config) {
+    let authorizationData = globalService.get("authorizationData");
+    config.headers = config.headers || {};
+
+    config.headers["__antiForgeryFormToken"] = angular
+      .element('input[name="__antiForgeryFormToken"]')
+      .attr("value");
+
+    if (authorizationData && authorizationData.access_token)
+      config.headers.Authorization = "Bearer " + authorizationData.access_token;
+
+    return config;
+  }
+  function responseError(rejection) {
+    if (rejection.status === 401) {
+      document.body.innerHTML =
+        '<div style="height: 100vh; line-height: 100vh; text-align: center; font-size: 18px;">زمان اعتبار شما به پایان رسیده، مجددا وارد سامانه شوید</div>';
+      $timeout(() => {
+        window.location.reload();
+      }, 2000);
     }
 
-    return service;
-
-    function request(config) {
-		let authorizationData = globalService.get('authorizationData');
-		config.headers = config.headers || {};
-
-		config.headers['__antiForgeryFormToken'] = angular.element('input[name="__antiForgeryFormToken"]').attr('value');
-
-		if (authorizationData && authorizationData.access_token)
-			config.headers.Authorization = 'Bearer ' + authorizationData.access_token;
-
-		return config;
-    }
-    function responseError(rejection) {
-        if (rejection.status === -1) {
-            return _ch(rejection);
-        }
-        else if (rejection.status === 0) {
-            //toastr.serviceUnavailable();
-        }
-        else if (rejection.status === 500) {
-            //toastr.internalServerError(rejection.data);
-        }
-        else if (rejection.status === 400) {
-            //toastr.badRequest(rejection.data.message);
-        } else if (rejection.status === 404) {
-            //toastr.notFound();
-        } else if (rejection.status === 401) {
-            if (rejection.data.type === 'AccessDeny') {
-                //toastr.unauthorized(rejection.data.message);
-            } else {
-                return _ch(rejection);
-            }
-        } else {
-            //toastr.statusError(rejection.status, rejection.statusText);
-        }
-
-        return $q.reject(rejection);
-    }
-    function _ch(rejection) {
-        var authService = $injector.get("authService");
-        var $state = $injector.get("$state");
-
-        var authData = $localStorage['authorizationData'];
-
-        if (authData) {
-            if (authData.useRefreshTokens) {
-                $state.go('auth.lockme');
-                return $q.reject(rejection);
-            }
-        }
-        authService.logOut();
-        $state.go('auth.signin');
-        return $q.reject(rejection);
-    }
+    return $q.reject(rejection);
+  }
 }
