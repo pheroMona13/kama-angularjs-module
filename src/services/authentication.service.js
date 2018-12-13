6b2f42f7d48620324043bce5654bc47315194a0a
Hadi Kefayati <h.kefaiati2010@gmail.com>
2018-12-13 15:16:02 +0330

    remove cookie usage

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,30 +1,21 @@
-﻿authenticationService.$inject = ['$cookies', 'globalService'];
-export default function authenticationService($cookies, globalService) {
-    var service = {
+﻿authenticationService.$inject = ['globalService'];
+export default function authenticationService(globalService) {
+    let service = {
         setCredentials: setCredentials
         , clearCredentials: clearCredentials
-        , setPosition: setPosition
         , isAuthenticated: isAuthenticated
     };
     
     return service;
 
     function setCredentials(responseToken) {
-        let expireDate = new Date(responseToken['.expires']);
-
-        globalService.set('authorizationData', responseToken); // does it need to be stored?
-        $cookies.put('access-token', responseToken.access_token, { 'expires': expireDate });
+        globalService.set('authorizationData', responseToken);
     }
     function clearCredentials() {
         globalService.remove('authorizationData');
         globalService.remove('currentUser');
         globalService.remove('currentUserPosition');
         globalService.remove('currentUserPositions');
-        $cookies.remove('access-token');
-        $cookies.remove('USER-Current-Position-ID');
-    }
-    function setPosition(position) {
-        $cookies.put('USER-Current-Position-ID', position.ID);
     }
     function isAuthenticated() {
         return (angular.element('input[name="__isAuthenticated"]').attr('value') === 'true');
